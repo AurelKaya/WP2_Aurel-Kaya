f0f7c851f187ba854624232f9a426a53527b012a
AurelKaya <90746710+AurelKaya@users.noreply.github.com>
2021-10-08 13:21:56 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Salsa</li>
+        <li>Nama Belakang : Aurel Kaya</li>
+        <li>Nama Lengkap : Salsa Aurel Kaya</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="square">
+        <li> Jl. Dr. Ratna. Jatikramat, Jatiasih, Kota Bekasi.</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="square">
+        <li> Bekasi, 23 Agustus 2002</li>
+    </ul>
+    <h4>Hobby</h4>
+    <ul type="square">
+        <li>Bermain Gitar</li>
+        <li>Bernyanyi, dan Menari.</li>
+    </ul>
+</section>
